1aca5d0760ec77ffe9439ed192d5fad1bac61b67
Zephyr <a1614206328@qq.com>
2025-09-26 18:52:29 +0800

    update

--- 2001/ze_surf_hp/boss_hp.js
+++ 2001/ze_surf_hp/boss_hp.js
@@ -1,7 +1,6 @@
 import { Instance } from "cs_script/point_script";
 
 let HEALTH_PLAYERADD = 999;
-
 let BOSS_HEALTH = 2000;
 let BOSS_MAX_HEALTH = 0;
 let HP_BAR_MAX_FRAME = 15;
@@ -18,7 +17,7 @@ let BOSS_HUD_IND = true;
 let BOSS_HUD_ST = "◼";
 let BOSS_HUD_ST2 = "◻";
 
-let TICKRATE_B = 0.01;
+const TICKRATE_B = 0.01;
 let IS_BOSS_FIGHT = false;
 
 let ITEM_DAMAGE = "";
@@ -35,21 +34,9 @@ function StartBoss() {
     HP_BAR_FRAME = HP_BAR_MAX_FRAME;
     
     IS_BOSS_FIGHT = true;
-
-    CheckHealth();
-};
-
-Instance.OnScriptInput("AddHealth", () => {
     
-    BOSS_HEALTH += HEALTH_PLAYERADD;
-    
-    if (BOSS_HEALTH > BOSS_MAX_HEALTH) {
-        BOSS_MAX_HEALTH = BOSS_HEALTH;
-        HP_PER_FRAME = BOSS_HEALTH / HP_BAR_MAX_FRAME;
-    }
-    
-    CheckHealth();
-});
+    Instance.SetThink(CheckHealth, Instance.GetGameTime() + TICKRATE_B);
+}
 
 function CheckHealth() {
     if (!IS_BOSS_FIGHT) return;
@@ -64,7 +51,7 @@ function CheckHealth() {
     }
 
     BuildHud();
-    Instance.SetNextThink(Instance.GetGameTime() + TICKRATE_B, CheckHealth);
+    Instance.SetNextThink(Instance.GetGameTime() + TICKRATE_B);
 }
 
 function BuildHud() {
@@ -96,8 +83,8 @@ function BuildHud() {
         BOSS_HUD_TEXT += ITEM_DAMAGE;
     }
     
-    if (BOSS_PERCENT_C && BOSS_PERCENT_C.length > 0) {
-        Instance.EntFireAtName(BOSS_PERCENT_C, "InValue", `${PERCENT_HP}`, 0);
+    if (BOSS_PERCENT_C) {
+        Instance.EntFireAtName(BOSS_PERCENT_C, "InValue", `${PERCENT_HP}`);
     }
     
     if (BOSS_HUD_IND) {
@@ -113,8 +100,41 @@ function BuildHud() {
         BOSS_HUD_TEXT += "]";
     }
     
-    Instance.EntFireAtName(BOSS_HUD_ENT, "SetMessage", BOSS_HUD_TEXT, 0);
+    Instance.EntFireAtName(BOSS_HUD_ENT, "SetMessage", BOSS_HUD_TEXT);
 }
+
+function BossKill() {
+    IS_BOSS_FIGHT = false;
+    Instance.EntFireAtName(BOSS_ENT, "FireUser3");
+    Instance.EntFireAtName(BOSS_HUD_ENT, "SetMessage", `${BOSS_NAME}: 0`);
+    Instance.EntFireAtName(BOSS_HUD_ENT, "HideHudHint", "", 0.1);
+    ResetBoss();
+}
+
+function ResetBoss() {
+    BOSS_HEALTH = 25;
+    BOSS_MAX_HEALTH = 25;
+    HP_BAR_MAX_FRAME = 10;
+    HP_BAR_FRAME = 10;
+    HP_PER_FRAME = 2.5;
+    IS_BOSS_FIGHT = false;
+    BOSS_HUD_IND = true;
+    ITEM_DAMAGE = "";
+    ITEM_DAMAGE_TICK = 2.00;
+    GRENADE_DAMAGE = 0;
+    GRENADE_DAMAGE_TICK = 2.00;
+}
+
+Instance.OnScriptInput("AddHealth", () => {
+    BOSS_HEALTH += HEALTH_PLAYERADD;
+    
+    if (BOSS_HEALTH > BOSS_MAX_HEALTH) {
+        BOSS_MAX_HEALTH = BOSS_HEALTH;
+        HP_PER_FRAME = BOSS_HEALTH / HP_BAR_MAX_FRAME;
+    }
+    
+    CheckHealth();
+});
 
 Instance.OnScriptInput("GrenadeDamage", () => {
     if (!IS_BOSS_FIGHT) return;
@@ -127,32 +147,23 @@ Instance.OnScriptInput("GrenadeDamage", () => {
     CheckHealth();
 });
 
-/*
-Instance.OnScriptInput("ItemDamage", (context) => {
+Instance.OnScriptInput("ItemDamage", (inputData) => {
     if (!IS_BOSS_FIGHT) return;
     
-    const arg = context.value || "item,0";
+    const arg = inputData.value?.toString() || "item,0";
     const arg_rs = arg.replace(/\s+/g, '');
     const arr = arg_rs.split(",");
     
-    const itemName = arr[0];
+    const itemName = arr[0] || "item";
     const damage = Number(arr[1]) || 0;
     
     BOSS_HEALTH = Math.max(0, BOSS_HEALTH - damage);
     const subs = damage < 0 ? "+" : "-";
     ITEM_DAMAGE = ` (${itemName}: ${subs}${Math.abs(damage)} HP)`;
+    ITEM_DAMAGE_TICK = 2.00;
     
     CheckHealth();
 });
-*/
-
-function BossKill() {
-    IS_BOSS_FIGHT = false;
-    Instance.EntFireAtName(BOSS_ENT, "FireUser3");
-    Instance.EntFireAtName(BOSS_HUD_ENT, "SetMessage", `${BOSS_NAME}: 0`);
-    Instance.EntFireAtName(BOSS_HUD_ENT, "HideHudHint", "", 0.1);
-    ResetBossS();
-}
 
 Instance.OnScriptInput("SubtractHealth", () => {
     if (!IS_BOSS_FIGHT || BOSS_HEALTH <= 0) return;
@@ -160,41 +171,23 @@ Instance.OnScriptInput("SubtractHealth", () => {
     CheckHealth();
 });
 
-Instance.OnScriptInput("BossKill", () => BossKill());
+Instance.OnScriptInput("BossKill", () => {
+    BossKill();
+});
 
-Instance.OnGameEvent("round_start", (event) => {
-    ResetBossS();
+Instance.OnRoundStart(() => {
+    ResetBoss();
     Instance.Msg('=== ROUND START ===');
 });
 
-function ResetBossS() {
-    BOSS_HEALTH = 25;
-    BOSS_MAX_HEALTH = 25;
-    HP_BAR_MAX_FRAME = 10;
-    HP_BAR_FRAME = 10;
-    HP_PER_FRAME = 2.5;
-    IS_BOSS_FIGHT = false;
-    BOSS_HUD_IND = true;
-    ITEM_DAMAGE = "";
-    ITEM_DAMAGE_TICK = 2.00;
-    GRENADE_DAMAGE = 0;
-    GRENADE_DAMAGE_TICK = 2.00;
-    
-}
-
-
-// boss_phys,boss_hud,Cubic,120,220
 Instance.OnScriptInput("Start_test_Boss", () => {
-    BOSS_ENT = "boss_phys"
-    BOSS_HUD_ENT = "boss_hud"
-
-    BOSS_NAME = "Cubic"
+    BOSS_ENT = "boss_phys";
+    BOSS_HUD_ENT = "boss_hud";
+    BOSS_NAME = "Cubic";
     BOSS_HEALTH = 20;
-    HEALTH_PLAYERADD = 50
-
+    HEALTH_PLAYERADD = 50;
     HP_BAR_MAX_FRAME = 10;
     HP_PER_FRAME = 0;
-
     ITEM_DAMAGE_TICK = 2.00;
     GRENADE_DAMAGE = 50;
     GRENADE_DAMAGE_TICK = 0.50;
@@ -202,19 +195,15 @@ Instance.OnScriptInput("Start_test_Boss", () => {
 });
 
 Instance.OnScriptInput("Start_Angry_Boss", () => {
-    BOSS_ENT = "angry_boss"
-    BOSS_HUD_ENT = "boss_hud"
-
-    BOSS_NAME = "Evil surf"
+    BOSS_ENT = "angry_boss";
+    BOSS_HUD_ENT = "boss_hud";
+    BOSS_NAME = "Evil surf";
     BOSS_HEALTH = 100;
-    HEALTH_PLAYERADD = 220
-
+    HEALTH_PLAYERADD = 220;
     HP_BAR_MAX_FRAME = 10;
     HP_PER_FRAME = 0;
-
     ITEM_DAMAGE_TICK = 2.00;
     GRENADE_DAMAGE = 50;
     GRENADE_DAMAGE_TICK = 0.50;
-
     StartBoss();
 });
