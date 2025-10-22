7cef80fa9d6accedd655f3e167ff4eca1a8dcebc
Zephyr <a1614206328@qq.com>
2025-10-22 20:59:35 +0800

    1

--- 2001/ze_minecraft_drifts/2en0w/item_manage.js
+++ 2001/ze_minecraft_drifts/2en0w/item_manage.js
@@ -2,47 +2,47 @@ import { Instance } from "cs_script/point_script";
 
 Instance.OnScriptInput("OnUse", (_input) => {
 
-		if(_input.caller.GetParent().GetParent() === _input.activator){
-			
-			Instance.EntFireAtTarget(_input.caller,"FireUser1", null,_input.caller,_input.activator)
+        if(_input.caller.GetParent().GetParent() === _input.activator){
+            
+            Instance.EntFireAtTarget(_input.caller,"FireUser1", null,_input.caller,_input.activator)
 
-		}
+        }
 
 });
 
 Instance.OnScriptInput("GiveAmmo", (_input) => {
-	   let _len = 90000
+       let _len = 90000
        let _wps = Instance.FindEntitiesByClass("weapon_*");
-	   let _pos = _input.caller.GetAbsOrigin();
+       let _pos = _input.caller.GetAbsOrigin();
 
-	   for (let _i = 0; _i < _wps.length; _i++) {
-			if(_len > vector3LengthSquare(vector3Sub(_wps[_i].GetAbsOrigin(),_pos))){
-				Instance.EntFireAtTarget(_wps[_i],"SetAmmoAmount","9999")
-			}
-			
-		}
+       for (let _i = 0; _i < _wps.length; _i++) {
+            if(_len > vector3LengthSquare(vector3Sub(_wps[_i].GetAbsOrigin(),_pos))){
+                Instance.EntFireAtTarget(_wps[_i],"SetAmmoAmount","9999")
+            }
+            
+        }
 
-		
-		
+        
+        
 });
 
 Instance.OnScriptInput("SetZombieHP", (_input) => {
-	   let _len = 150000
+       let _len = 150000
        let _p = Instance.FindEntitiesByClass("player");
-	   let _pos = _input.caller.GetAbsOrigin();
+       let _pos = _input.caller.GetAbsOrigin();
 
-	   for (let _i = 0; _i < _p.length; _i++) {
-			if(_p[_i].GetTeamNumber()==2&&_len > vector3LengthSquare(vector3Sub(_p[_i].GetAbsOrigin(),_pos))){
-				_p[_i].SetHealth(300);
-				_p[_i].SetMaxHealth(300);
-			}
-			
-		}	
+       for (let _i = 0; _i < _p.length; _i++) {
+            if(_p[_i].GetTeamNumber()==2&&_len > vector3LengthSquare(vector3Sub(_p[_i].GetAbsOrigin(),_pos))){
+                _p[_i].SetHealth(300);
+                _p[_i].SetMaxHealth(300);
+            }
+            
+        }   
 });
 function vector3Sub(_vec1,_vec2){
-	
-	let _sum = { x: _vec1.x-_vec2.x, y: _vec1.y-_vec2.y, z: _vec1.z-_vec2.z };
-	return _sum;
+    
+    let _sum = { x: _vec1.x-_vec2.x, y: _vec1.y-_vec2.y, z: _vec1.z-_vec2.z };
+    return _sum;
 }
 
 function vector3LengthSquare(_vec) {
