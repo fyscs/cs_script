d2b94b6f4c1ecfe4a2204693bf43ab41edad9bea
Zephyr <a1614206328@qq.com>
2025-10-22 20:58:05 +0800

    4

--- 2001/ze_ancient_wrath_p/fog.js
+++ 2001/ze_ancient_wrath_p/fog.js
@@ -1,24 +1,24 @@
 import { Instance } from 'cs_script/point_script';
 
-let script_name = "script_fog";
+const script_name = "script_fog";
 let fog_start_dist = 2000;
 let fog_end_dist = 3200;
 let fog = false;
 Instance.OnScriptInput("StartFog", () => {
     fog = true;
-    Instance.EntFireAtName("Train_fog1", "Enable", "", 0);
+    Instance.EntFireAtName({ name: "Train_fog1", input: "Enable" });
     set_fog();
 });
 function set_fog() {
     if (!fog)
         return;
-    Instance.EntFireAtName("Train_fog1", "SetFogStartDistance", fog_start_dist, 0);
-    Instance.EntFireAtName("Train_fog1", "SetFogEndDistance", fog_end_dist, 0);
+    Instance.EntFireAtName({ name: "Train_fog1", input: "SetFogStartDistance", value: fog_start_dist });
+    Instance.EntFireAtName({ name: "Train_fog1", input: "SetFogEndDistance", value: fog_end_dist });
     fog_start_dist -= 4;
     fog_end_dist -= 4;
     if (fog_start_dist < 400)
         return;
-    Instance.EntFireAtName(script_name, "RunScriptInput", "SetFog", 0.01);
+    Instance.EntFireAtName({ name: script_name, input: "RunScriptInput", value: "SetFog", delay: 0.01 });
 }
 Instance.OnScriptInput("SetFog", set_fog);
 Instance.OnScriptInput("ResetScript", () => {
