69522a0f97549556ec98c0b776d82e0ab003b5df
Zephyr <a1614206328@qq.com>
2025-11-21 18:14:23 +0800

    add ze_funny_runner (#44)

---
+++ 2001/ze_funny_runner/entityrandomspawn2.js
@@ -0,0 +1,41 @@
+import { Instance } from "cs_script/point_script";
+
+const spawnentity_script_ent = "entityrandom_script2";
+let origin = null;
+
+function RandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+Instance.OnScriptInput ("RandomSpawn",({caller}) => {
+    if(caller?.IsValid())
+    {
+        if(origin == null)
+        {
+            origin = caller?.GetAbsOrigin();
+        }
+        const randomOffset = {
+            x: 0,
+            y: RandomInt(-896, 896),
+            z: 0
+        };
+            
+        const spawnPos = {
+            x: origin.x + randomOffset.x,
+            y: origin.y + randomOffset.y,
+            z: origin.z + randomOffset.z
+        };
+            
+        caller?.ForceSpawn(spawnPos, { pitch: 0, yaw: RandomInt(0, 360), roll: 0 });
+    }
+});
+
+Instance.OnScriptInput("StartSpawn", ({caller}) => {
+    Instance.EntFireAtName({ name: spawnentity_script_ent, input: "runscriptinput", value: "RandomSpawn", delay: 0.00, caller: caller });
+});
+
+Instance.OnRoundStart(() => {
+    origin = null;
+});
