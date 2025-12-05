2f90ec6ae546924f386f36ba179bf247292d5340
Zephyr <a1614206328@qq.com>
2025-12-05 12:51:49 +0800

    1205

--- 2001/ze_last_man_standing/kou_script.js
+++ 2001/ze_last_man_standing/kou_script.js
@@ -3,12 +3,12 @@ import { Instance, CSPlayerPawn, CSPlayerController, Entity } from "cs_script/po
 export const utils = {
     printl(a) {Instance.Msg(a)},
 
-    EntFire(name = "", input = "", value = "", delay = 0.0, caller = undefined, activator = undefined)
+    EntFire(name = "", input = "", value = "", delay = 0.00, caller = undefined, activator = undefined)
     {
         Instance.EntFireAtName({name,input,value,delay,caller,activator})
     },
 
-    EntFireByHandle(target = "", input = "", value = "", delay = 0.0, caller = undefined, activator = undefined)
+    EntFireByHandle(target = "", input = "", value = "", delay = 0.00, caller = undefined, activator = undefined)
     {
         if(target == undefined) return;
         Instance.EntFireAtTarget({target,input,value,delay,caller,activator})
@@ -93,55 +93,106 @@ function handleLastManStanding() {
     const randomIndex = Math.floor(Math.random() * aliveCTPlayers.length);
     const selectedPlayer = aliveCTPlayers[randomIndex];
     selectedPlayer.SetEntityName("last_man_standing");
-    utils.EntFire("last_man_standing","Alpha","255",0);
-    utils.EntFire("last_man_standing","Color","255 255 255",0);
-    utils.EntFire("last_man_standing","SetDamageFilter","",0);
-    utils.EntFire("last_man_standing","ClearContext","",0);
-    utils.EntFire("last_man_standing","KeyValues","speed 1",0);
-    utils.EntFire("last_man_standing","KeyValues","gravity 1",0);
-    utils.EntFire("human_mech","Alpha","255",0);
-    utils.EntFire("human_mech","Color","255 255 255",0);
-    utils.EntFire("human_mech","SetDamageFilter","",0);
-    utils.EntFire("human_mech","ClearContext","",0);
-    utils.EntFire("human_mech","KeyValues","speed 1",0);
-    utils.EntFire("human_mech","KeyValues","gravity 1",0);
-    utils.EntFire("monster","Alpha","255",0);
-    utils.EntFire("monster","Color","255 255 255",0);
-    utils.EntFire("monster","SetDamageFilter","",0);
-    utils.EntFire("monster","ClearContext","",0);
-    utils.EntFire("monster","KeyValues","speed 1",0);
-    utils.EntFire("monster","KeyValues","gravity 1",0);
-    utils.EntFire("_human_item_tank_health_drivver_zombiekill","Enable","",0.02);
-    utils.EntFire("_human_item_tank_health_drivver_zombiekill","Trigger","",0.04);
-    utils.EntFire("v_model1","Fireuser3","",0.06);
-    utils.EntFire("Human_Item_Mech_UI","Deactivate","",0.02);
-    utils.EntFire("Human_Item_Mech_Cam","DisableCamera","",0.02);
-    utils.EntFire("Human_Item_Mech_Attack_*","Kill","",0.04);
-    utils.EntFire("Human_Item_Mech_Hurt_*","Kill","",0.04);
-    utils.EntFire("Human_Item_Mech_Model","SetAnimation","Die",0.04);
-    utils.EntFire("Human_Item_Mech_Model","ClearParent",0.04);
-    utils.EntFire("Human_Item_Mech_Selector","Disable",0.04);
-    utils.EntFire("Human_Item_Mech_Sound_Die","StartSound",0.04);
-    utils.EntFire("Human_Item_Mech_UI","Kill",0.04);
-    utils.EntFire("Human_Item_Mech_Body","Break",0.1);
-    utils.EntFire("Human_Item_Mech*","Kill","",2.50);
-    utils.EntFire("wpn_jugger_phys","Break","",0.02);
-    utils.EntFire("wpn_jugger_ui","Kill","",0.02);
-    utils.EntFire("Zombie_Item_Summoner_Slow_Trigger","Kill","",0.02);
-    utils.EntFire("Zombie_Item_Summoner_Teleport*","Kill","",0.02);
-    utils.EntFire("stage5_goaway","FireUser1","",0.20);
-    utils.EntFire("lms_fuckoffzombies","Enable","",0.20);
-    utils.EntFire("stage_5_last_man_trigger","Enable","",0.20);
-    utils.EntFire("teleport_lastmanstanding","Enable","",0.50);
-    utils.EntFire("lms_fuckoffzombies","Disable","",0.60);
-    utils.EntFire("teleport_lastmanstanding","Disable","",0.60);
-    utils.EntFire("stage_5_last_man_trigger","Kill","",2.20);
-    utils.EntFire("last_man_reset","Enable","",0.50);
-    utils.EntFire("last_man_reset","Trigger","",1.50);
-    utils.EntFire("last_man_reset","Trigger","",2.50);
+    
+    Instance.SetNextThink(Instance.GetGameTime() + 0.02);
+    bExecuteLastManEvents = true;
+}
+
+let bExecuteLastManEvents = false;
+
+function executeLastManEvents() {
+    // last_man_standing
+    utils.EntFire("last_man_standing", "Alpha", "255", 0);
+    utils.EntFire("last_man_standing", "Color", "255 255 255", 0);
+    utils.EntFire("last_man_standing", "SetDamageFilter", "", 0);
+    utils.EntFire("last_man_standing", "ClearContext", "", 0);
+    utils.EntFire("last_man_standing", "KeyValues", "speed 1", 0);
+    utils.EntFire("last_man_standing", "KeyValues", "gravity 1", 0);
+    
+    // human_mech
+    utils.EntFire("human_mech", "Alpha", "255", 0);
+    utils.EntFire("human_mech", "Color", "255 255 255", 0);
+    utils.EntFire("human_mech", "SetDamageFilter", "", 0);
+    utils.EntFire("human_mech", "ClearContext", "", 0);
+    utils.EntFire("human_mech", "KeyValues", "speed 1", 0);
+    utils.EntFire("human_mech", "KeyValues", "gravity 1", 0);
+    
+    // monster
+    utils.EntFire("monster", "Alpha", "255", 0);
+    utils.EntFire("monster", "Color", "255 255 255", 0);
+    utils.EntFire("monster", "SetDamageFilter", "", 0);
+    utils.EntFire("monster", "ClearContext", "", 0);
+    utils.EntFire("monster", "KeyValues", "speed 1", 0);
+    utils.EntFire("monster", "KeyValues", "gravity 1", 0);
+    
+    // 坦克
+    utils.EntFire("v_button1", "Kill", "", 0);
+    utils.EntFire("v_model1", "Fireuser3", "", 0.04);
+    
+    // 机甲
+    utils.EntFire("Human_Item_Mech_UI", "Deactivate", "", 0.02);
+    utils.EntFire("Human_Item_Mech_UI", "Kill", "", 0.04);
+    utils.EntFire("Human_Item_Mech_Cam", "Kill", "", 0.02);
+    utils.EntFire("Human_Item_Mech_Attack_*", "Disable", "", 0.02);
+    utils.EntFire("Human_Item_Mech_Attack_*", "Kill", "", 0.04);
+    utils.EntFire("Human_Item_Mech_Hurt_*", "Disable", "", 0.02);
+    utils.EntFire("Human_Item_Mech_Hurt_*", "Kill", "", 0.04);
+    utils.EntFire("Human_Item_Mech_Model", "SetAnimation", "Die", 0.02);
+    utils.EntFire("Human_Item_Mech_Model", "ClearParent", "", 0.04);
+    utils.EntFire("Human_Item_Mech_Model", "Kill", "", 2.50);
+    utils.EntFire("Human_Item_Mech_Selector", "Disable", "", 0.04);
+    utils.EntFire("Human_Item_Mech_Sound_Die", "StartSound", "", 0.04);
+    utils.EntFire("Human_Item_Mech_Body", "Break", "", 0.02);
+    utils.EntFire("Human_Item_Mech*", "Kill", "", 0.2);
+
+    // 机械狗
+    utils.EntFire("wpn_jugger_phys", "Break", "", 0.02);
+    utils.EntFire("wpn_jugger_ui", "Kill", "", 0.02);
+    utils.EntFire("wpn_jugger*", "Kill", "", 0.04);
+    
+    // 黑洞巫师
+    utils.EntFire("Zombie_Item_Summoner_Slow_Trigger", "Kill", "", 0.02);
+    utils.EntFire("Zombie_Item_Summoner_Teleport*", "Kill", "", 0.02);
+    
+    // 传送&处死
+    utils.EntFire("stage5_goaway", "FireUser1", "", 0.20);
+    utils.EntFire("lms_fuckoffzombies", "Enable", "", 0.20);
+    utils.EntFire("stage_5_last_man_trigger", "Enable", "", 0.20);
+    utils.EntFire("teleport_lastmanstanding", "Enable", "", 0.30);
+    utils.EntFire("lms_fuckoffzombies", "Disable", "", 0.50);
+    utils.EntFire("teleport_lastmanstanding", "Disable", "", 0.50);
+    utils.EntFire("stage_5_last_man_trigger", "Kill", "", 2.20);
+    
+    //last_man_reset_relay
+    utils.EntFire("last_man_reset", "Enable", "", 0.50);
+    utils.EntFire("last_man_reset", "Trigger", "", 1.50);
+    utils.EntFire("last_man_reset", "Trigger", "", 2.50);
+}
+
+function handleNowReset() {
+    executeLastManEvents();
+}
+
+function extendedMainLoop() {
+    if (bIsRunning) {
+        removeTArmorFromAllPlayers();
+    }
+    
+    if (bExecuteLastManEvents) {
+        bExecuteLastManEvents = false;
+        executeLastManEvents();
+    }
+    
+    if (bIsRunning) {
+        Instance.SetNextThink(Instance.GetGameTime() + 3.0);
+    } else if (bExecuteLastManEvents) {
+        Instance.SetNextThink(Instance.GetGameTime() + 0.01);
+    }
 }
 
 Instance.OnScriptInput("Start", startArmorRemover);
 Instance.OnScriptInput("Stop", stopArmorRemover);
 Instance.OnScriptInput("LastManStanding", handleLastManStanding);
-Instance.SetThink(mainLoop);
+Instance.OnScriptInput("NowReset", handleNowReset);
+
+Instance.SetThink(extendedMainLoop);
