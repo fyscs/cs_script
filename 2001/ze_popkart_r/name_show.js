2c64e7c684e9f2a4bdabdd1c4bebf6f06ed81d7c
PiPiCat233 <cccczz@foxmail.com>
2025-12-07 13:41:53 +0800

    Update ze_popkart_r (#51)
    * Update kart.js
    
    ·开启严格检查并修复部分问题
    
    * Update name_show.js
    
    ·开启严格检查并修复部分问题
    
    * Create weapon_strip.js
    
    ·新增武器剥离脚本，在进行赛车关卡时强制剥离玩家武器
    
    * Update weapon_strip.js
    
    ·强制剥离匕首，防止玩家集体使用匕首划车整蛊
    
    * Delete weapon_strip.js
    
    ·已使用其他方案解决武器对卡丁车造成击退的问题

--- 2001/ze_popkart_r/name_show.js
+++ 2001/ze_popkart_r/name_show.js
@@ -1,8 +1,15 @@
-import { Instance } from "cs_script/point_script";
+import { Instance, CSPlayerPawn } from "cs_script/point_script";
+
+/**
+ * 冠军名称显示脚本
+ * 此脚本由皮皮猫233编写
+ * 2025/11/23
+ */
 
 Instance.OnScriptInput("nameShow", (context) => {
-    const player = context.activator;
-    if (!player || !player.IsValid())
-        return;
-    Instance.EntFireAtName({ name: "kart_1st_text", input: "SetMessage", value: player.GetPlayerController().GetPlayerName() });
+    const player = /** @type {CSPlayerPawn|undefined} */ (context.activator);
+    if (!player || !player.IsValid()) return;
+    const playerController = player.GetPlayerController();
+    if (!playerController || !playerController.IsValid()) return;
+    Instance.EntFireAtName({ name: "kart_1st_text", input: "SetMessage", value: playerController.GetPlayerName() });
 });
