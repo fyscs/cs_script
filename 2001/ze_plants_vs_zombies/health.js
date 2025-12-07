3c1765a9313335e115ca638ceee3dbbbcca809f1
PiPiCat233 <cccczz@foxmail.com>
2025-12-07 13:41:48 +0800

    Update ze_plants_vs_zombies (#50)
    * Update health.js
    
    ·修正错误的逻辑判断
    
    * Create item_squash.js
    
    ·新增窝瓜神器脚本
    
    * Update item_squash.js
    
    ·微调窝瓜行为参数并添加角度回正

--- 2001/ze_plants_vs_zombies/health.js
+++ 2001/ze_plants_vs_zombies/health.js
@@ -3,19 +3,15 @@ import { Instance } from "cs_script/point_script";
 /**
  * 血量恢复功能
  * 此脚本由皮皮猫233编写
- * 2025/11/3
+ * 2025/11/24
  */
 
 Instance.OnScriptInput("Health", (inputData) => {
-    if (!inputData.activator || !inputData.activator.IsValid()) {
-        return;
-    }
+    if (!inputData.activator || !inputData.activator.IsValid()) return;
     
     // 检查是否为玩家
     const className = inputData.activator.GetClassName();
-    if (!className || !className === "player") {
-        return;
-    }
+    if (!(className === "player")) return;
     
     const currentHealth = inputData.activator.GetHealth();
     inputData.activator.SetHealth(currentHealth + 2500);
