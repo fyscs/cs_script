bcf5ee2c2b7221f67bb2bf140d4a190b0bb80494
PiPiCat233 <cccczz@foxmail.com>
2025-12-27 20:01:51 +0800

    1227
    ·新增炮台索敌逻辑优化脚本

---
+++ 2001/ze_plants_vs_zombies/target_pick.js
@@ -0,0 +1,124 @@
+import { Instance, Entity } from "cs_script/point_script";
+
+/**
+ * 炮台植物目标挑选脚本
+ * 用于使炮台植物可以锁定最近的目标
+ * 此脚本由皮皮猫233编写
+ * 2025/12/27
+ */
+
+const plantState = new Map();
+
+Instance.OnScriptInput("Trigger", (inputData) => {
+    const player = inputData.activator;
+    if (!player || !player.IsValid()) return;
+    if (!inputData.caller || !inputData.caller.IsValid()) return;
+    const entityName = inputData.caller.GetEntityName();
+
+    const suffix = GetSuffix(entityName);
+    if (!suffix) return;
+    const plantType = GetPlantType(entityName);
+    if (!plantType) return;
+
+    if (plantState.has(plantType + suffix)) {
+        plantState.get(plantType + suffix).push(player);
+    } else {
+        plantState.set(plantType + suffix, [player]);
+    }
+});
+
+Instance.OnScriptInput("PickTarget", (inputData) => {
+    if (!inputData.caller || !inputData.caller.IsValid()) return;
+    const entityName = inputData.caller.GetEntityName();
+
+    const suffix = GetSuffix(entityName);
+    if (!suffix) return;
+    const plantType = GetPlantType(entityName);
+    if (!plantType) return;
+
+    if (!plantState.has(plantType + suffix)) return;
+    const players = plantState.get(plantType + suffix);
+    plantState.delete(plantType + suffix);
+
+    // 获取请求挑选的植物位置
+    const plantPhy = Instance.FindEntityByName("item_" + plantType + "_phy_" + suffix);
+    if (!plantPhy || !plantPhy.IsValid()) return;
+    const plantPosition = plantPhy.GetAbsOrigin();
+
+    const closestPlayer = FindClosestPlayer(players, plantPosition);
+    if (!closestPlayer || !closestPlayer.IsValid()) return;
+
+    if (plantType === "cobcannon") {
+        Instance.EntFireAtName({ 
+            name: "item_cobcannon_boom_maker", 
+            input: "ForceSpawnAtEntityOrigin",
+            value: "!activator",
+            activator: closestPlayer,
+        });
+    } else {
+        Instance.EntFireAtName({ 
+            name: "item_" + plantType + "_orient_" + suffix, 
+            input: "SetTarget",
+            value: "!activator",
+            activator: closestPlayer,
+        });
+    }
+});
+
+Instance.OnRoundStart(() => {
+    plantState.clear();
+});
+
+/**
+ * 寻找距离植物位置水平距离最短的玩家
+ * @param {Array<Entity>} players - 玩家对象数组
+ * @param {import("cs_script/point_script").Vector} plantPosition - 植物位置坐标
+ * @returns {Entity|null} 距离最短的玩家对象，如果没有玩家则返回 null
+ */
+function FindClosestPlayer(players, plantPosition) {
+  let closestPlayer = null;
+  let shortestDistance = Infinity;
+  
+  for (let i = 0; i < players.length; i++) {
+    const player = players[i];
+    const playerPos = player.GetAbsOrigin();
+    
+    // 计算水平距离
+    const dx = playerPos.x - plantPosition.x;
+    const dy = playerPos.y - plantPosition.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    
+    // 更新最近玩家
+    if (distance < shortestDistance) {
+    shortestDistance = distance;
+    closestPlayer = player;
+    }
+  }
+  return closestPlayer;
+}
+
+/**
+ * 获取植物种类
+ * @param {string} entityName 
+ * @returns {string|null}
+ */
+function GetPlantType(entityName) {
+    const suffix = entityName.split('_');
+    if (suffix.length > 0) {
+        return suffix[1];
+    }
+    return null;
+}
+
+/**
+ * 获取name fixup自动生成的后缀
+ * @param {string} entityName 
+ * @returns {string|null}
+ */
+function GetSuffix(entityName) {
+    const suffix = entityName.split('_');
+    if (suffix.length > 0) {
+        return suffix[suffix.length - 1];
+    }
+    return null;
+}
