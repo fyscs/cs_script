b7526c89817d78728b6f45d144de3d24771e3e55
Zephyr <a1614206328@qq.com>
2026-01-26 20:18:10 +0800

    update ze_minecraft_drifts (#88)

--- 2001/ze_minecraft_drifts/2en0w/stage_manage.js
+++ 2001/ze_minecraft_drifts/2en0w/stage_manage.js
@@ -1,74 +1,74 @@
 import { Instance } from "cs_script/point_script";
 
-let failNum = -1
+let failNum = 0
 let stageNum = 1
 
 Instance.OnRoundStart((event) => {
-        getItemTemp();
-        switch(stageNum){
-            case 1:triggerStage1();break
-            case 2:triggerStage2();break
-            
-        }
-        
+		getItemTemp();
+		switch(stageNum){
+			case 1:triggerStage1();break
+			case 2:triggerStage2();break
+			
+		}
+		
 });
 
 Instance.OnScriptInput("SetStage1", (_input) => {
-        stageNum = 1    
+		stageNum = 1	
 });
 Instance.OnScriptInput("SetStage2", (_input) => {
-        stageNum = 2    
+		stageNum = 2	
 });
 
 Instance.OnRoundEnd((event) => {
 
-    if(event.winningTeam == 2){ 
-        failNum++
-    }
+	if(event.winningTeam == 2){	
+		failNum++
+	}
 });
 
 
 function triggerStage1(){
-    
-    Instance.EntFireAtName("st1_start_01","trigger")
-    for (let _i = failNum; _i > 0; _i--) {
-        item_ammo_tem.ForceSpawn({x:-12481 ,y:-11475 ,z:4290})
-        item_book_tem.ForceSpawn({x:-12481 ,y:-11539 ,z:4290})
-    
-    }
-    
+	
+	Instance.EntFireAtName("st1_start_01","trigger")
+	for (let _i = failNum; _i > 0; _i--) {
+		item_ammo_tem.ForceSpawn({x:-12481 ,y:-11475 ,z:4290})
+		item_book_tem.ForceSpawn({x:-12481 ,y:-11539 ,z:4290})
+	
+	}
+	
 }
 
 function triggerStage2(){
-    
-    Instance.EntFireAtName("st1_start_02","trigger")
-    for (let _i = failNum; _i > 0; _i--) {
-        item_ammo_tem.ForceSpawn({x:-7223 ,y:-5983 ,z:1010})
-        item_book_tem.ForceSpawn({x:-7223 ,y:-5862 ,z:1010})
-    
-    }
+	
+	Instance.EntFireAtName("st1_start_02","trigger")
+	for (let _i = failNum; _i > 0; _i--) {
+		item_ammo_tem.ForceSpawn({x:-7223 ,y:-5983 ,z:1010})
+		item_book_tem.ForceSpawn({x:-7223 ,y:-5862 ,z:1010})
+	
+	}
 }
 
 let item_ammo_tem
 let item_book_tem
 
 function getItemTemp(){
-    item_ammo_tem = Instance.FindEntityByName("item_ammo_tem");
-    item_book_tem = Instance.FindEntityByName("item_book_tem"); 
+	item_ammo_tem = Instance.FindEntityByName("item_ammo_tem");
+	item_book_tem = Instance.FindEntityByName("item_book_tem");	
 }
 
 Instance.OnScriptInput("Checkhuman20", (_input) => {
-    
-    let _pl = Instance.FindEntitiesByClass("player");
-    let _human = 0
-    for (let _i = 0; _i < _pl.length; _i++) {
-        if(_pl[_i].GetTeamNumber()==3){
-            _human++
-        }
-    }
-    Instance.Msg(_pl.length)
-    if(_human<20){
-        Instance.EntFireAtName("st1_bgm_005","stopsound")
-        Instance.EntFireAtName("st1_bgm_006","startsound")
-    }
+	
+	let _pl = Instance.FindEntitiesByClass("player");
+	let _human = 0
+	for (let _i = 0; _i < _pl.length; _i++) {
+		if(_pl[_i].GetTeamNumber()==3){
+			_human++
+		}
+	}
+	Instance.Msg(_pl.length)
+	if(_human<20){
+		Instance.EntFireAtName("st1_bgm_005","stopsound")
+		Instance.EntFireAtName("st1_bgm_006","startsound")
+	}
 });
