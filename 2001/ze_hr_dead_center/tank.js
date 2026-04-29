2b1738d8c385cd0ed7049c29e134a5e0f27e9565
PiPiCat233 <cccczz@foxmail.com>
2026-04-29 23:51:29 +0800

    0429
    ·新增受击减速机制

--- 2001/ze_hr_dead_center/tank.js
+++ 2001/ze_hr_dead_center/tank.js
@@ -48,6 +48,16 @@ Instance.OnScriptInput("Die", () => {
     currentTank = undefined;
 });
 
+Instance.OnScriptInput("Hit", () => {
+    if (currentTank && currentTank.IsValid()) {
+        const velocity = currentTank.GetAbsVelocity();
+        const speed = Math.hypot(velocity.x, velocity.y);
+        if (speed >= 100) {
+            currentTank.Teleport({ velocity: LimitHorizontalMagnitude(velocity, Math.max((speed - 20), 100))});
+        }
+    }
+});
+
 Instance.OnRoundStart(() => {
     if (currentTank && currentTank.IsValid()) {
         Instance.EntFireAtTarget({ target: currentTank, input: "SetDamageFilter", value: "" });
@@ -82,13 +92,36 @@ function BecomeTank(pawn) {
     // @ts-ignore
     const entities = Instance.FindEntityByName("tank_temp").ForceSpawn(position, { pitch: 0, yaw: angles.yaw, roll: 0 });
     for (const entity of entities) {
-        if (entity.GetEntityName() === "tank_ui") {
+        const entityName = entity.GetEntityName();
+        if (entityName === "tank_ui") {
             Instance.EntFireAtTarget({ target: entity, input: "Activate", activator: currentTank });
-        } else {
+        } else if (entityName === "tank_yell_sound") {
+            Instance.EntFireAtTarget({ target: entity, input: "StartSound" });
+        } else if (entityName !== "tank_phy_mm" && entityName !== "tank_phy" && entityName !== "tank_walk_sound" && entityName !== "tank_walk_sound_loop_timer") {
             Instance.EntFireAtTarget({ target: entity, input: "SetParent", value: "!activator", activator: currentTank });
         }
     }
     Instance.EntFireAtTarget({ target: currentTank, input: "SetDamageFilter", value: "god" });
     Instance.EntFireAtTarget({ target: currentTank, input: "Alpha", value: 0 });
     Instance.ServerCommand("say >> " + currentTank.GetPlayerController()?.GetPlayerName() + " << 成为了Tank!!!");
+}
+
+/**
+ * 限制三维向量的水平分量（X和Y）的模长，若超过 maxMagnitude 则等比例缩放，保持方向不变；垂直分量 Z 保持不变。
+ * @param {import("cs_script/point_script").Vector} v - 输入向量
+ * @param {number} maxMagnitude - 允许的最大水平模长（非负数）
+ * @returns {import("cs_script/point_script").Vector} 限制后的新向量
+ */
+function LimitHorizontalMagnitude(v, maxMagnitude) {
+    const { x, y, z } = v;
+    const horizMag = Math.hypot(x, y);
+    if (horizMag <= maxMagnitude) {
+        return { x, y, z };
+    }
+    const scale = maxMagnitude / horizMag;
+    return {
+        x: x * scale,
+        y: y * scale,
+        z: z
+    };
 }
