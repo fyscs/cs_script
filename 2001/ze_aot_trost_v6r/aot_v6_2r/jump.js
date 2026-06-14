445d4fb62fe84c23e8b1173cbb2d3064cfa79326
PiPiCat233 <cccczz@foxmail.com>
2026-06-14 16:51:40 +0800

    0614
    ·调整数值

--- 2001/ze_aot_trost_v6r/aot_v6_2r/jump.js
+++ 2001/ze_aot_trost_v6r/aot_v6_2r/jump.js
@@ -3,13 +3,13 @@ import { Instance } from "cs_script/point_script";
 /**
  * 高跳脚本
  * 此脚本由皮皮猫233编写
- * 2026/6/9
+ * 2026/6/14
  */
 
 Instance.OnScriptInput("Jump", (inputData) => {
     const player = inputData.activator;
     if (!player || !player.IsValid()) return;
     const velocity = player.GetAbsVelocity();
-    velocity.z = 680;
+    velocity.z = 650;
     player.Teleport({ velocity });
 });
