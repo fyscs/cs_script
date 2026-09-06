6eda42bf26da778d1e07bbb8c2cae77e96bf7b94
Aaron <45914779+notkoen@users.noreply.github.com>
2026-09-06 08:49:16 -0700

    Update cosmo script (#239)

--- 2001/ze_ffvii_cosmo_canyon_v5_p/cosmo_patch.js
+++ 2001/ze_ffvii_cosmo_canyon_v5_p/cosmo_patch.js
@@ -1583,6 +1583,6 @@ function traceLine(start, target) {
 }
 Instance.SetNextThink(Instance.GetGameTime());
 Instance.SetThink(() => {
-    Instance.SetNextThink(Instance.GetGameTime() + 0.1);
+    Instance.SetNextThink(Instance.GetGameTime());
     runSchedulerTick();
 });
